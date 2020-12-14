b672c69ddb52e89fb9f7b64df25d29d4901ed885
GeunTae KIM <60951051+Rootstar07@users.noreply.github.com>
2020-12-14 20:45:15 +0900

    Video1

---
+++ Pong/Assets/Scripts/PaddleMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMove : MonoBehaviour
+{
+
+    float speed = 30;
+
+    private void FixedUpdate()
+    {
+
+        float vertPress = Input.GetAxisRaw("Vertical");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, vertPress) * speed;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
